397376acc34adbaad67000715bd5a9a94a528587
Sharad Singh Kushwaha <sharadsingh0203@gmail.com>
2024-05-13 23:16:37 +0530

    Learning Python

---
+++ conditionalstatement1.py
@@ -0,0 +1,12 @@
+# if condition:
+# statement1
+# statement2
+# Here if the condition is true, if block
+# will consider only statement1 to be inside its block.
+i = 10
+if(i > 15):
+    print("I am Guru")
+print("I am Shishya")
+
+# Output : I am Shishya
+
---
+++ conditionalstatement2.py
@@ -0,0 +1,16 @@
+# if (condition):
+#     # Executes this block if
+#     # condition is true
+# else:
+#     # Executes this block if
+#     # condition is false
+
+i = "Sharad"
+if(i == "Sharad"):
+    print("Andar aa jaao")
+    print("You're in if Block")
+else:
+    print("Bahar jaao")
+    print("You're in else Block")
+    
+# Output : Andar aa jaao You're in if Block
