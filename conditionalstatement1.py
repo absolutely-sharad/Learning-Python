725e5ffcab458761be1c0c94fbea2ff0b0ca7ef8
Sharad Singh Kushwaha <153629616+absolutely-sharad@users.noreply.github.com>
2024-07-16 23:40:11 +0530

    Update conditionalstatement1.py

--- conditionalstatement1.py
+++ conditionalstatement1.py
@@ -3,7 +3,7 @@
 # statement2
 # Here if the condition is true, if block
 # will consider only statement1 to be inside its block.
-i = 10
+i = 12
 if(i > 15):
     print("I am Guru")
 print("I am Shishya")
